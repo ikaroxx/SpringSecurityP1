f4969a068526b22369f651f8eb713b6f47a3c08a
KONGWONGYU <kwg9790@naver.com>
2023-12-15 00:29:00 +0900

    권한 설정

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/login").permitAll()
+                        .requestMatchers("/", "/login","/loginProc","/join","/joinProc").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
                         .anyRequest().authenticated()
--- src/main/java/com/example/demo/controller/JoinController.java
+++ src/main/java/com/example/demo/controller/JoinController.java
@@ -1,13 +1,20 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.JoinDTO;
+import com.example.demo.service.JoinService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class JoinController {
-
+    
+    @Autowired
+    //나중에 생성자로 바꿔서 주입받자
+    private JoinService joinService;
+    
+    
     @GetMapping("/join")
     public String joinP(){
 
@@ -20,6 +27,7 @@ public class JoinController {
         
         // 출력체크
         System.out.println(joinDTO.getUsername());
+        joinService.joinProcess(joinDTO);
 
 
         return "redirect:/login";
