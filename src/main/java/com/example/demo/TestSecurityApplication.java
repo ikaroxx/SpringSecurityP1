73864b50da38bc20c9db264e8618c911be79aa19
KONGWONGYU <kwg9790@naver.com>
2023-12-19 02:50:29 +0900

    세션확인까지

--- src/main/java/com/example/demo/TestSecurityApplication.java
+++ src/main/java/com/example/demo/TestSecurityApplication.java
@@ -8,6 +8,7 @@ public class TestSecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestSecurityApplication.class, args);
+
 	}
 
 }
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -6,4 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
+    boolean   existsByUsername(String username);
+
+
+    UserEntity findByUsername(String username);
+
 }
